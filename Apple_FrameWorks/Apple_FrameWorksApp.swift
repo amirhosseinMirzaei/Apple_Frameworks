44022d76ed7ab50a125de9f1d5621b467ffb0fcd
AmirHossein <amirhossein1mirzaei79@gmail.com>
2025-06-02 23:59:48 -0700

    Initial Commit

---
+++ Apple_FrameWorks/Apple_FrameWorksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameWorksApp.swift
+//  Apple_FrameWorks
+//
+//  Created by Amirhossein Mirzaei on 6/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameWorksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
