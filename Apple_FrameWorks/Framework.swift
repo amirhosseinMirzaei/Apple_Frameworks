621f54565778670f9081c98e2cc2aa3d12b0dc5d
AmirHossein <amirhossein1mirzaei79@gmail.com>
2025-06-03 01:24:53 -0700

    initial commit

---
+++ Apple_FrameWorks/Framework.swift
@@ -0,0 +1,8 @@
+//
+//  Framework.swift
+//  Apple_FrameWorks
+//
+//  Created by Amirhossein Mirzaei on 6/3/25.
+//
+
+import Foundation
---
+++ Apple_FrameWorks/FrameworkGridView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkGridView.swift
+//  Apple_FrameWorks
+//
+//  Created by Amirhossein Mirzaei on 6/3/25.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkGridView()
+}
