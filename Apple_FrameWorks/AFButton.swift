cfb2d7df75e05ee40e402ef5dfd81106e0a152ab
AmirHossein <amirhossein1mirzaei79@gmail.com>
2025-06-03 02:37:55 -0700

    detail screen

---
+++ Apple_FrameWorks/AFButton.swift
@@ -0,0 +1,18 @@
+//
+//  AFButton.swift
+//  Apple_FrameWorks
+//
+//  Created by Amirhossein Mirzaei on 6/3/25.
+//
+
+import SwiftUI
+
+struct AFButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AFButton()
+}
---
+++ Apple_FrameWorks/FrameworkDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkDetailView.swift
+//  Apple_FrameWorks
+//
+//  Created by Amirhossein Mirzaei on 6/3/25.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkDetailView()
+}
